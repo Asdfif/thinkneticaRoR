1b803544197e3af99a8cc0f8217b8294ca10e698
Asdfif <sasha-burov@mail.ru>
2020-11-05 20:37:54 +0300

    Внесены поправки к заданию

--- st_tr_rt.rb
+++ st_tr_rt.rb
@@ -7,28 +7,19 @@ class Station
 	end
 
 	def show_all_trains
-		@trains.each { |train| puts train.name}
-		puts ''
-	end
+		@trains.each { |train| }
+		end
 
 	def show_type_trains(type)
-		@trains.each { |train| 
+		@trains.each do |train| 
 			if train.type == type
-			puts train.name
-			end }
-		puts ''
+			train.name
+			end 
+		end
 	end	
 
 	def send_train(train)
-		puts "Куда отправить поезд? f/b?"
-		q = gets.chomp
-		if q == 'f'
-			train.forward
-			@trains.delete(train)
-		elsif q == 'b'
-			train.back
-			@trains.delete(train)
-		end
+		@trains.delete(train)
 	end	
 end
 
@@ -36,36 +27,23 @@ class Route
 
 	attr_accessor  :stations
 	def initialize(first, terminal)
-		if first.class == Station && terminal.class == Station
-			@stations = [first, terminal]
-		end
+		@stations = [first, terminal]
 	end
 
 	def add_station(station)
-		unless @stations.include?(*station)
-		@stations.insert(-2, *station)
-		puts ''
+		unless @stations.include?(station)
+		@stations.insert(-2, station)
 		end
 	end
 
 	def remove_station(station)
-		station.trains.each { |trains| 
-			if trains.route != self 
-				if station != @stations[0] && station != @stations[-1]
-				@stations.delete(station)
-				else
-					puts 'Нельзя удалить терминальные станции маршрута' 
-				end
-				puts ''
-			else 
-				puts 'Нельзя удалить станцию, поскльку поезд с этого маршрута пребывает на ней'
-			end }
-			puts ''
+		if @stations.include?(station) && station != @stations[0] && station != @stations[-1]
+			@stations.delete(station)
+		end
 	end
 
 	def show_all_stations
-		@stations.each { |station| puts station.name}
-		puts ''
+		@stations.each { |station| station.name}
 	end
 end
 
@@ -77,13 +55,11 @@ class Train
 		@type = type
 		@vagons = vagons
 		@current_speed = 0
-		@route = nil
-		@current_station = nil
 	end
 
 	def faster (speed)
 		@current_speed += speed
-		puts "Текущая скорость - #{@current_speed}"
+		@current_speed
 	end
 	
 	def slower (speed)
@@ -91,36 +67,34 @@ class Train
 		if @current_speed <= 0
 			@current_speed =0
 		end
-		puts "Текущая скорость - #{@current_speed}"
+		@current_speed
 	end
 	
-	def vagons; puts " Количество вагонов - #{@vagons}"; end
+	def vagons
+		@vagons
+	end
 
 	def add_vagon
 		if @current_speed == 0
 			@vagons += 1
-		else 
-			puts "Снизьте скорость до нуля! А затем добавляйте вагоны!"
 		end
 	end
 
 	def del_vagon
 		if @current_speed == 0 && @vagons >= 1
 			@vagons -= 1
-		else 
-			puts "Вагонов не оставлось!"
 		end
 	end
 
-	def speed; puts "Текущая скорость - #{@current_speed}"; end
+	def speed
+		@current_speed
+	end
 
 	def set_route(route)
 		@route = route
 		@current_station = route.stations[0]
 		@route.stations[0].trains << self
-		@route.stations.each { |station| puts station.name}
-		puts ''
-	end
+		end
 
 	def forward 
 		i = @route.stations.index(@current_station)
@@ -128,10 +102,7 @@ class Train
 			@current_station = @route.stations[i += 1]
 			@route.stations[i].trains << self
 			@route.stations[i-1].trains.delete(self)
-			
-			puts @current_station.name
-		else
-			puts "Конечная станция"
+			@current_station.name
 		end
 	end
 
@@ -141,13 +112,11 @@ class Train
 			@current_station = @route.stations[i -= 1]
 			@route.stations[i].trains << self
 			@route.stations[i+1].trains.delete(self)
-			puts @current_station.name
-		else
-			puts "Конечная станция"
+			@current_station.name
 		end
 	end
 
 	def current_station
-		puts @current_station.name
+		@current_station.name
 	end
 end

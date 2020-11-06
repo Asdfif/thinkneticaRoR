585da214ffa47d291c8b86065d00f0e225ae0b78
Asdfif <sasha-burov@mail.ru>
2020-11-06 22:37:19 +0300

    Update st_tr_rt.rb
    Внесены поправки к заданию (2): поправки от рубокопа,  добавлены методы нахождения следующей и предыдущей станций, изменены методы перемещения поезда вперед и назад.

--- st_tr_rt.rb
+++ st_tr_rt.rb
@@ -1,122 +1,110 @@
 class Station
+  attr_accessor :trains, :name, :type
 
-	attr_accessor :trains , :name, :type
-	def initialize(name)
-		@name = name
-		@trains = []
-	end
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
 
-	def show_all_trains
-		@trains.each { |train| }
-		end
+  def show_type_trains(type)
+    @trains.each do |train|
+      train if train.type == type
+    end
+  end
 
-	def show_type_trains(type)
-		@trains.each do |train| 
-			if train.type == type
-			train.name
-			end 
-		end
-	end	
-
-	def send_train(train)
-		@trains.delete(train)
-	end	
+  def send_train(train)
+    @trains.delete(train)
+  end
 end
 
 class Route
+  attr_accessor :stations
 
-	attr_accessor  :stations
-	def initialize(first, terminal)
-		@stations = [first, terminal]
-	end
+  def initialize(first, terminal)
+    @stations = [first, terminal]
+  end
 
-	def add_station(station)
-		unless @stations.include?(station)
-		@stations.insert(-2, station)
-		end
-	end
+  def add_station(station)
+    @stations.insert(-2, station) unless @stations.include?(station)
+  end
 
-	def remove_station(station)
-		if @stations.include?(station) && station != @stations[0] && station != @stations[-1]
-			@stations.delete(station)
-		end
-	end
-
-	def show_all_stations
-		@stations.each { |station| station.name}
-	end
+  def remove_station(station)
+    @stations.delete(station) if @stations.include?(station) && station != @stations[0] && station != @stations[-1]
+  end
 end
 
 class Train
+  attr_accessor :name, :type, :stations, :route, :current_speed
 
-	attr_accessor :name , :type, :stations, :route
-	def initialize(name, type, vagons = 0)
-		@name = name
-		@type = type
-		@vagons = vagons
-		@current_speed = 0
-	end
+  def initialize(name, type, vagons = 0)
+    @name = name
+    @type = type
+    @vagons = vagons
+    @current_speed = 0
+  end
 
-	def faster (speed)
-		@current_speed += speed
-		@current_speed
-	end
-	
-	def slower (speed)
-		@current_speed -= speed
-		if @current_speed <= 0
-			@current_speed =0
-		end
-		@current_speed
-	end
-	
-	def vagons
-		@vagons
-	end
+  def faster(speed)
+    @current_speed += speed
+    @current_speed
+  end
 
-	def add_vagon
-		if @current_speed == 0
-			@vagons += 1
-		end
-	end
+  def slower(speed)
+    @current_speed -= speed
+    @current_speed = 0 if @current_speed <= 0
+    @current_speed
+  end
 
-	def del_vagon
-		if @current_speed == 0 && @vagons >= 1
-			@vagons -= 1
-		end
-	end
+  attr_reader :vagons, :current_station
 
-	def speed
-		@current_speed
-	end
+  def add_vagon
+    @vagons += 1 if @current_speed.zero?
+  end
 
-	def set_route(route)
-		@route = route
-		@current_station = route.stations[0]
-		@route.stations[0].trains << self
-		end
+  def del_vagon
+    @vagons -= 1 if @current_speed.zero? && @vagons >= 1
+  end
 
-	def forward 
-		i = @route.stations.index(@current_station)
-		if  i < (@route.stations.size - 1)
-			@current_station = @route.stations[i += 1]
-			@route.stations[i].trains << self
-			@route.stations[i-1].trains.delete(self)
-			@current_station.name
-		end
-	end
+  def speed
+    @current_speed
+  end
 
-	def back 
-		i = @route.stations.index(@current_station)
-		if  i > 0
-			@current_station = @route.stations[i -= 1]
-			@route.stations[i].trains << self
-			@route.stations[i+1].trains.delete(self)
-			@current_station.name
-		end
-	end
+  def set_route(route)
+    @route = route
+    @current_station = route.stations[0]
+    @route.stations[0].trains << self
+  end
 
-	def current_station
-		@current_station.name
-	end
+  def next_station
+    i = @route.stations.index(@current_station)
+    return unless i < (@route.stations.size - 1)
+
+    @route.stations[i + 1]
+  end
+
+  def previous_station
+    i = @route.stations.index(@current_station)
+    return unless i.positive?
+
+    @route.stations[i - 1]
+  end
+
+  def forward
+    return unless next_station
+
+    i = @route.stations.index(@current_station)
+    @current_station = @route.stations[i += 1]
+    @route.stations[i].trains << self
+    @route.stations[i - 1].trains.delete(self)
+    @current_station
+  end
+
+  def back
+    return unless previous_station
+
+    i = @route.stations.index(@current_station)
+    @current_station = @route.stations[i -= 1]
+    @route.stations[i].trains << self
+    @route.stations[i + 1].trains.delete(self)
+    @current_station
+  end
 end
